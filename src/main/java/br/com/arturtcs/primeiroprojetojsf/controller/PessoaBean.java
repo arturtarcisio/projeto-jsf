85367984320d7056060273483f959e7591df76ce
Artur Tarcisio <arturtarcisio1@gmail.com>
2020-07-17 14:56:32 -0300

    Consumindo o RESTful do CEP - Web Service Cep - Parte 2

--- src/main/java/br/com/arturtcs/primeiroprojetojsf/controller/PessoaBean.java
+++ src/main/java/br/com/arturtcs/primeiroprojetojsf/controller/PessoaBean.java
@@ -1,5 +1,10 @@
 package br.com.arturtcs.primeiroprojetojsf.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,17 +42,17 @@ public class PessoaBean {
 	private void mostrarMsg(String msg) {
 		FacesContext context = FacesContext.getCurrentInstance();
 		FacesMessage message = new FacesMessage(msg);
-		context.addMessage(null, message);		
+		context.addMessage(null, message);
 	}
 
 	public String novo() {
-		//Executa algum processo antes de limpar
+		// Executa algum processo antes de limpar
 		pessoa = new Pessoa();
 		return "";
 	}
-	
+
 	public String limpar() {
-		//Executa algum processo antes de limpar
+		// Executa algum processo antes de limpar
 		pessoa = new Pessoa();
 		return "";
 	}
@@ -59,40 +64,58 @@ public class PessoaBean {
 		mostrarMsg("Removido com sucesso!");
 		return "";
 	}
-	
-	public String logar() {		
+
+	public String logar() {
 		Pessoa pessoaUser = iDaoPessoa.consultarUsuario(pessoa.getLogin(), pessoa.getSenha());
-		
-		if(pessoaUser != null) {
-			//adicionar o usuário na sessão usuarioLogado
+
+		if (pessoaUser != null) {
+			// adicionar o usuário na sessão usuarioLogado
 			FacesContext context = FacesContext.getCurrentInstance();
 			ExternalContext externalContext = context.getExternalContext();
 			externalContext.getSessionMap().put("usuarioLogado", pessoaUser);
-			
+
 			return "primeirapagina.jsf";
-		}		
-		
+		}
+
 		return "index.jsf";
 	}
-	
+
 	public boolean permiteAcesso(String acesso) {
 		FacesContext context = FacesContext.getCurrentInstance();
 		ExternalContext external = context.getExternalContext();
 		Pessoa pessoa = (Pessoa) external.getSessionMap().get("usuarioLogado");
-		
+
 		return pessoa.getPerfilUser().equals(acesso);
 	}
-	
+
 	public void pesquisaCep(AjaxBehaviorEvent event) {
-		System.out.println("Metodo pesquisa cep chamado. CEP: " + pessoa.getCep());
+		try {
+			URL url = new URL("https://viacep.com.br/ws/" + pessoa.getCep() + "/json/"); // monta url
+			URLConnection connection = url.openConnection(); // abre conexao
+			
+			InputStream is = connection.getInputStream(); // executa lá do lado do servidor do ws
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+			String cep = "";
+			StringBuilder jsonCep = new StringBuilder();
+			
+			while ((cep = br.readLine()) != null) {
+				jsonCep.append(cep);
+			}
+
+			System.out.println(jsonCep);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			mostrarMsg("Erro ao consultar o cep");
+		}
 	}
-	
 
 	/*
-	 * Sempre quando abrir a tela que o managed bean for
-	 * instanciado, após ser criado em memória ele vai carregar
-	 * o metodo que está com esta anotação @PostConstruct
-	*/
+	 * Sempre quando abrir a tela que o managed bean for instanciado, após ser
+	 * criado em memória ele vai carregar o metodo que está com esta
+	 * anotação @PostConstruct
+	 */
 	@PostConstruct
 	public void carregarListaDePessoas() {
 		listaDePessoas = dao.listarTodos(Pessoa.class);
